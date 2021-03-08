48f1352a1188794b1ac65437bac5af9265def608
Daniel <cdmx1963@amerike.edu.mx>
2021-03-08 11:00:02 -0600

    Examen :c

--- Tec/Assets/Scripts/Circle.cs
+++ Tec/Assets/Scripts/Circle.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour
+public class Circle
 {
-   
+    public int numberOfEdges = 0;
+    public int radious = 10;
+
+    public void ShowCircle()
+    {
+        Debug.Log("tu circulo tiene vertices =" + numberOfEdges);
+        Debug.Log("tu circulo tiene un radio de:" + radious);
+    }
+    
 }
--- Tec/Assets/Scripts/Controller.cs
+++ Tec/Assets/Scripts/Controller.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller
+public class Controller : MonoBehaviour
 {
+    public int chooseFigure = Random.Range(1, 5);
 
+    void Update()
+    {
+        if(chooseFigure == 1)
+        {
+            Circle.ShowCircle();
+        }
+    } 
 }
