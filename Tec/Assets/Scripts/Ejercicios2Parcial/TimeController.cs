068b11fc4ba75681fd1128415d5e20c00d0cabd6
Daniel <cdmx1963@amerike.edu.mx>
2021-03-16 20:59:39 -0600

    El ejercicio 3 esta casi completo, solo falta el botón pero ya me canse

---
+++ Tec/Assets/Scripts/Ejercicios2Parcial/TimeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    Timer timeController = new Timer(0, 0, 0);
+
+    public int changeTime;
+    public float scaleTime = 1;
+
+    private float timeFrameScale = 0f;
+    private float timeShowSecond = 0f;
+    private float scaleInitialTime;
+
+    void Start()
+    {
+        scaleInitialTime = scaleTime;
+        timeShowSecond = changeTime;
+        CountTime(changeTime);
+    }
+    void Update()
+    {
+        timeFrameScale = Time.deltaTime * scaleTime;
+        timeShowSecond += timeFrameScale;
+        CountTime(timeShowSecond);
+    }
+    public void CountTime(float timeInSeconds)
+    {
+        timeController.GetSecond = 0;
+        timeController.GetMinute = 0;
+        timeController.GetHour = 0;
+
+        timeController.GetMinute = (int)timeInSeconds / 60;
+        timeController.GetSecond = (int)timeInSeconds & 60;
+        timeController.GetHour = (int)timeInSeconds / 3600;
+    }
+    public void ShowTime()
+    {
+        Debug.Log(timeController.GetHour + " hr " + timeController.GetMinute + " min " + timeController.GetSecond + " sec ");
+    }
+}
---
+++ Tec/Assets/Scripts/Ejercicios2Parcial/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer 
+{
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Timer(int ahour, int aminute, int asecond)
+    {
+        hour = ahour;
+        minute = aminute;
+        second = asecond;
+    }
+
+    public int GetHour
+    {
+        get { return hour; }
+        set { hour = value; }
+    }
+    public int GetMinute
+    {
+        get { return minute; }
+        set { minute = value; }
+    }
+    public int GetSecond
+    {
+        get { return second; }
+        set { second = value; }
+    }
+}
